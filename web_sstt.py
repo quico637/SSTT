5a1e229022da71afd13099c74c33d106f3ef733f
Francisco Arnaldo Boix Martinez <quicomurciai@gmail.com>
2022-02-16 19:27:11 +0100

    Update web_sstt.py

--- web_sstt.py
+++ web_sstt.py
@@ -225,9 +225,9 @@ def process_web_request(cs, webroot):
                 print(respuesta)
                 enviar_recurso(r_solicitado, os.stat(r_solicitado).st_size, respuesta, cs)
                 print("HE LLEGAO AL FINAL")
-                #print(data)
-                #cerrar_conexion(cs)
-                #sys.exit()
+
+                cerrar_conexion(cs)
+                sys.exit()
 
     except Exception:
         print("Han cerrao el socket lo mas probable")
