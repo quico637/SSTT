3b94b2920025ba337668d560d7eec27884dcb27d
Francisco Arnaldo Boix Martinez <quicomurciai@gmail.com>
2022-03-04 15:11:48 +0100

    Update web_sstt.py

--- web_sstt.py
+++ web_sstt.py
@@ -122,7 +122,7 @@ def process_cookies(headers):
     """
     
     cookie = False
-    val = ""
+    val = -1
 
     for i in headers:
         print(i)
@@ -135,7 +135,7 @@ def process_cookies(headers):
                 cookie = True
                 print("SE ha encontrado cookie_counter")
                 print(i)
-                val = i.split(sep="=", maxsplit=-1)[1]
+                val = int(i.split(sep="=", maxsplit=-1)[1])
                 #res = er_cookie.fullmatch(i)
                 #val = res.group(3)
                 #val =
